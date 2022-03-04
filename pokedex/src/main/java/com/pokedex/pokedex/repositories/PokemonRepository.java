79f65d014c60c92c1e7fa5cb762674ab9b84c71e
Pozos <pablo-emanuel.pozos@atos.net>
2022-03-04 16:47:37 -0600

    Avance

--- pokedex/src/main/java/com/pokedex/pokedex/repositories/PokemonRepository.java
+++ pokedex/src/main/java/com/pokedex/pokedex/repositories/PokemonRepository.java
@@ -1,13 +1,13 @@
 package com.pokedex.pokedex.repositories;
 
 import com.pokedex.pokedex.models.Pokemon;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
 //Repository Pattern
 @Repository
-public interface PokemonRepository extends CrudRepository<Pokemon,Long> {
+public interface PokemonRepository extends JpaRepository<Pokemon,Long> {
     public abstract ArrayList<Pokemon> findByNombre(Integer categoria);
 }
